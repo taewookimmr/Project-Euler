fa4d3a23e09e0e57a6a0971c03e0347f55b43355
taewookimmr <taewookimmr@gmail.com>
2019-12-27 20:52:32 +0900

    problem-4 updated

--- 4/4_largestPalindrome.py
+++ 4/4_largestPalindrome.py
@@ -1,5 +1,6 @@
 def solution():
-    def isPalindrome(x):
+    
+    def isPalindrome_not_efficient(x):
         tempstr = [e for e in str(x)]
         revstr = reversed(tempstr)
         check = sum([0 if i==j else 1 for i,j in zip(tempstr, revstr)])
@@ -7,14 +8,28 @@ def solution():
             return False 
         else :
             return True
-
-    answer=[]
-    for i in range(999, 99, -1):
-        for j in range(i, 99, -1):
-            if isPalindrome(i*j):
-                answer.append([i*j, i, j])
-                
-    answer.sort()
-    print(answer[-1])
+        
+    def isPalindrome_efficient(x):
+        tempstr=[e for e in str(x)]
+        n = len(tempstr)
+        for i in range(n//2):
+            if tempstr[i] != tempstr[-(i+1)]:
+                return False
+        return True
+         
+    def body():
+        answer=[]
+        for i in range(999, 99, -1):
+            for j in range(i, 99, -1):
+                if isPalindrome_efficient(i*j):
+                    answer.append([i*j, i, j])
+                    
+        answer.sort()
+        print(answer[-1])
+    
+    body()
+    
 solution()        
-            
+
+
+##             
